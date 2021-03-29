3cdb3830f76802a69872abeeb4f66778753801fb
suramyaiiht <79524334+suramyaiiht@users.noreply.github.com>
2021-03-29 11:14:45 +0530

    Add files via upload

---
+++ staffSelection/src/main/java/yaksha/staffSelection/Main.java
@@ -0,0 +1,42 @@
+package yaksha.staffSelection;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+public class Main {
+	
+	public static int totalCalculation (Applicant app)
+	{
+		int total=0;
+		
+		// Your code for total marks calculation
+		
+			 return total;
+	}
+	 public static int percentageCalculation (int total)
+	 {
+				 // Your code to percentage calculation
+		 
+		 return 0;
+	 }
+    public static void main( String[] args )
+    {
+        Scanner scanner=new Scanner(System.in);
+        System.out.println("Enter number of applicants:");
+        int no=Integer.parseInt(scanner.nextLine());
+        ArrayList<Applicant> applicantList=new ArrayList<Applicant>();
+        
+        // Your code to accept all applicants' details, invoke different functions where required
+        
+         
+         
+         Collections.sort(applicantList);
+         
+        
+         // Your code to display all applicants in proper order
+         
+       
+        	
+    }
+
+
+}
---
+++ staffSelection/src/test/java/yaksha/staffSelection/MainClassTest.java
@@ -0,0 +1,45 @@
+package yaksha.staffSelection;
+
+import static yaksha.staffSelection.TestUtils.*;
+import org.junit.Test;
+
+
+/**
+ * Unit test for simple App.
+ */
+public class MainClassTest {
+	@Test
+	public void testTotalCalculation() throws Exception {
+		Applicant app1=new Applicant("John",90, 25, 75);
+		Applicant app2=new Applicant("Rio",55, 80, 75);
+		int val1=Main.totalCalculation(app1);
+		int val2=Main.totalCalculation(app2);
+		yakshaAssert(currentTest(),(val1==0?"true":"false"),businessTestFile);
+		yakshaAssert(currentTest(),(val2==210?"true":"false"),businessTestFile);
+		
+	}
+
+	@Test
+	public void testPercentageCalculation() throws Exception {
+		Applicant app1=new Applicant("John",90, 25, 75);
+		Applicant app2=new Applicant("Rio",55, 80, 75);
+		int tot1=Main.totalCalculation(app1);
+		int tot2=Main.totalCalculation(app2);
+		int val1=Main.percentageCalculation(tot1);
+		int val2=Main.percentageCalculation(tot2);
+		System.out.println(""+val1+val2);
+		yakshaAssert(currentTest(),(val1==0?"true":"false"),businessTestFile);
+		yakshaAssert(currentTest(),(val2==70?"true":"false"),businessTestFile);
+	}
+	
+	@Test
+    public void testExceptionCondition() throws Exception{
+	 yakshaAssert(currentTest(),true,boundaryTestFile);
+      }
+
+	@Test
+	public void testBoundaryCondition() throws Exception {
+	  yakshaAssert(currentTest(),true,exceptionTestFile);
+   }
+
+}
